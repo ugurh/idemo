403831b32738b76805a9ec28343f733f7744f4ad
harun ugur <ugur.harun@yandex.com>
2022-07-01 23:27:25 +0300

    Service Discovery

---
+++ service-discovery/src/test/java/io/ugurh/eureka/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ugurh.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
